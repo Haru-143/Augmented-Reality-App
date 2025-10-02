8a3f399906087778f80fd1e094f4f3c51550e5c9
Haru-143 <143gaeulpink@gmail.com>
2025-10-02 13:20:29 +0800

    add bottom navigation bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,37 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor:Colors.amber[50],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        unselectedItemColor: Colors.brown[700],
+        backgroundColor:Colors.amber[100],
+        iconSize: 35,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home'
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite'
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart'
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile'
+          ),
+        ],
+      ),
+    );
   }
 }
