7bfe3ca55fde570746ab08cce8791b6f1a29834d
Haru-143 <143gaeulpink@gmail.com>
2025-10-04 11:18:03 +0800

    add app bar and text field

--- lib/pages/home_page_content.dart
+++ lib/pages/home_page_content.dart
@@ -5,8 +5,44 @@ class HomePageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          'Furniture Store',
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Search bar
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color:Colors.grey[100],
+                  border: Border.all(color: Colors.grey[900]!),
+                  borderRadius: BorderRadius.circular(18),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search',
+                    prefixIcon: Icon(Icons.search),
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+                    border: InputBorder.none,
+                  ),
+                  textAlignVertical: TextAlignVertical.center,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
